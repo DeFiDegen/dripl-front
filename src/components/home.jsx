fbf88d14ce4a7a166f62a256a21b311fa348d33e
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-07-23 10:09:03 -0400

    added images for about section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link, withRouter } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <div className="banner" style={{ backgroundImage: "url('../../assets/images/homeBackgroundArt.jpeg')" }}>
-        <button type="button" id="call-to-action-btn">Dripl your art</button>
+        <Link to="/create">
+          <button type="button" id="call-to-action-btn">Dripl your art</button>
+        </Link>
+      </div>
+      <div className="about-section">
+        <div className="about-item">
+          <p>Upload a beat</p>
+          <img src="../../assets/images/driplMusicIcon.jpg" alt="" className="home-icon" />
+        </div>
+          <p>Upload an image</p>
+        <div className="about-item">
+          <img src="../../assets/images/driplPhotoIcon.png" alt="" className="home-icon" />
+        </div>
+        <div className="about-item">
+          <p>Dripl</p>
+          <img src="../../assets/images/driplDripIcon.png" alt="" className="home-icon" />
+        </div>
       </div>
     </>
   );
 };
 
-export default Home;
+export default withRouter(Home);
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,32 +2,21 @@ import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 
 const Navbar = () => {
-  const createClickHandle = () => {
-    console.log('Create BTN clicked');
-  };
-
-  const contactClickHandle = () => {
-    console.log('Contact BTN clicked');
-  };
-
-  const homeClickHandle = () => {
-    console.log('Home BTN clicked');
-  };
 
   return (
     <div className="navbar">
       <div className="left-nav">
         <Link to="/">
-          <img src="../../assets/images/navLogo.jpg" alt="" className="logo" onClick={homeClickHandle} />
-          <button type="button" onClick={homeClickHandle}>Dripl</button>
+          <img src="../../assets/images/navLogo.jpg" alt="" className="logo" />
+          <button type="button">Dripl</button>
         </Link>
       </div>
       <div className="right-nav">
         <Link to="/create">
-          <button type="button" onClick={createClickHandle}>Create</button>
+          <button type="button">Create</button>
         </Link>
         <Link to="/contact">
-          <button type="button" onClick={contactClickHandle}>Contact</button>
+          <button type="button">Contact</button>
         </Link>
       </div>
     </div>
