c5a10ed00eb685433e04c2b5c0653e30d22af711
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-07-23 08:48:26 -0400

    integrated router within app.jsx file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './home';
+import Navbar from './navbar';
+import Contact from './contact';
+import Create from './create';
 
 const App = () => {
   return (
-    <Home />
+    <Router>
+      <div className="box">
+        <Navbar />
+      </div>
+      <Switch>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/contact" exact component={() => <Contact />} />
+        <Route path="/create" exact component={() => <Create />} />
+      </Switch>
+    </Router>
   );
 };
 
