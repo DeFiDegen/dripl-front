3dbff70339dd88aa629eda85ba01142fffc991ce
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-07-23 11:02:37 -0400

    added logic for navigation. Selected tab now indicated through .active class assignement

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ location }) => {
 
   return (
     <div className="navbar">
       <div className="left-nav">
         <Link to="/">
           <img src="../../assets/images/navLogo.jpg" alt="" className="logo" />
-          <button type="button">Dripl</button>
+          <button type="button" className={`${location.pathname === "/" ? "active" : ""}`}>Dripl</button>
         </Link>
       </div>
       <div className="right-nav">
         <Link to="/create">
-          <button type="button">Create</button>
+          <button type="button" className={`${location.pathname === "/create" ? "active" : ""}`}>Create</button>
         </Link>
         <Link to="/contact">
-          <button type="button">Contact</button>
+          <button type="button" className={`${location.pathname === "/contact" ? "active" : ""}`}>Contact</button>
         </Link>
       </div>
     </div>
