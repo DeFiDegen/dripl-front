b21be0a2f273b87b2c5bac74e32c3cf221e5fabc
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-07-23 09:08:47 -0400

    fixed home page background image issue by injecting css directly inside the home.jsx file, on the div.banner

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <div className="banner">
+      <div className="banner" style={{ backgroundImage: "url('../../assets/images/homeBackgroundArt.jpeg')" }}>
         <button type="button" id="call-to-action-btn">Dripl your art</button>
-        {/* <img id="home-img" src="../../assets/images/homeBackgroundArt.jpeg" alt="" /> */}
       </div>
     </>
   );
