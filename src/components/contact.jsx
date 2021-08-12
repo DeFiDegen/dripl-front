2b13665abb4bd883835cf630bf03d09ec223d15a
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-08-12 08:33:05 -0400

    Contact page is tested and wokrs with emailjs

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,9 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-alert */
+/* eslint-disable class-methods-use-this */
+
 import React from "react";
+import emailjs from "emailjs-com";
 
 class Contact extends React.Component {
   constructor(props) {
@@ -22,10 +27,17 @@ class Contact extends React.Component {
     this.setState({ message: event.target.value });
   }
 
-  // handleSubmit funciton needs to be changed in order to handle post request to server
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
+    emailjs.sendForm('Dripl - Contact Page', 'template_edp48fo', event.target, 'user_c1XSQZnKnV3t0QkiJmWPA')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    event.target.reset();
+    alert("Your message has been successfully sent!");
   }
 
   render() {
@@ -33,18 +45,18 @@ class Contact extends React.Component {
     return (
       <div className="box">
         <h2>Contact</h2>
-        <form id="contact-form" method="POST" onSubmit={this.handleSubmit.bind(this)}>
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)}>
           <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" value={name} onChange={this.onNameChange.bind(this)} />
+            <label>Name</label>
+            <input type="text" className="form-control" name="name" value={name} onChange={this.onNameChange.bind(this)} />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" value={email} onChange={this.onEmailChange.bind(this)} />
+            <label>Email address</label>
+            <input type="email" className="form-control" name="email" value={email} onChange={this.onEmailChange.bind(this)} />
           </div>
           <div className="form-group" id="form-mesage">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5" value={message} onChange={this.onMessageChange.bind(this)} />
+            <label>Message</label>
+            <textarea className="form-control" rows="5" name="message" value={message} onChange={this.onMessageChange.bind(this)} />
           </div>
           <button type="submit">Submit</button>
         </form>
