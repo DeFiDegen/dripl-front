62a40974ca3861405336b9ab64c3871dd29f0a50
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-08-20 10:06:12 -0400

    layout completed, turned function component into class component for state use

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
-const Create = () => {
-  return (
-    <div>
-      TODO - Create
-    </div>
-  );
-};
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoUploaded: false,
+      songUploaded: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="box">
+        <div className="image-box">
+          <button type="button" id="upload-btn">Upload Image</button>
+        </div>
+        <div className="song-box">
+          <button type="button" id="upload-btn">Upload Music</button>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default Create;
