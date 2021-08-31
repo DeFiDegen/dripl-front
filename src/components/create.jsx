a07b5b57dbc1d30409a863a785520791911baba1
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-08-31 10:03:49 -0400

    added onChange handle for music and image state in create component

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -4,20 +4,37 @@ class Create extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoUploaded: false,
-      songUploaded: false,
+      selectedImage: null,
+      selectedMusic: null
     };
+  }
+
+  handleImage = (event) => {
+    const image = event.target.files[0];
+    this.setState({
+      selectedImage: image
+    });
+    console.log(this.state);
+  }
+
+  handleMusic = (event) => {
+    const music = event.target.files[0];
+    this.setState({
+      selectedMusic: music
+    });
+    console.log(this.state);
+  }
+
+  handleSubmit = () => {
+    console.log(this.state);
   }
 
   render() {
     return (
       <div className="box">
-        <div className="image-box">
-          <button type="button" id="upload-btn">Upload Image</button>
-        </div>
-        <div className="song-box">
-          <button type="button" id="upload-btn">Upload Music</button>
-        </div>
+        <input type="file" name="image" onChange={this.handleImage} />
+        <input type="file" name="music" onChange={this.handleMusic} />
+        <button type="submit" onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
